83330b3ea3e5aa93d8a46bd7f69e5d7d3193b859
Omer faruk Kavak <okavak@k1m42s01.42istanbul.com.tr>
2022-11-15 14:22:44 +0300

    updated

--- rank02/level1/ulstr/ulstr.c
+++ rank02/level1/ulstr/ulstr.c
@@ -1,18 +1,17 @@
 #include <unistd.h>
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	int	i;
+	int i = 0;
 
-	i = 0;
 	if (ac == 2)
 	{
-		while (av[1][i] != '\0')
+		while (av[1][i])
 		{
 			if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				av[1][i] = 219 - av[1][i];
+				av[1][i] -= 32;
 			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				av[1][i] = 155 - av[1][i];
+				av[1][i] += 32;
 			write(1, &av[1][i], 1);
 			i++;
 		}
